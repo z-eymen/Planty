79f4f9e5edc80b3474e07c622377d46a416850f0
zeliha <z_eymen@hotmail.com>
2023-12-29 00:15:30 +0100

    add page 'nous retrouvons' and creat css/theme.css file

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -4,4 +4,5 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent_style', get_template_directory_uri() . 'style.css');
+    wp_enqueue_style('theme_style', get_stylesheet_directory_uri() . 'css/theme.css', array(), filemtime(get_stylesheet_directory() . 'css/theme.css'));
 } 
