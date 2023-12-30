23b47cfe2c53c3dec713f5d8d4d1ea4ee7de150a
zeliha <z_eymen@hotmail.com>
2023-12-30 23:10:33 +0100

    Add css codes to navBar and the page 'commander'

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -1,8 +1,41 @@
 <?php
+/**
+ * Astra Child Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Astra Child
+ * @since 1.0.0
+ */
 
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles'); 
-function theme_enqueue_styles()
-{
-    wp_enqueue_style('parent_style', get_template_directory_uri() . 'style.css');
-    wp_enqueue_style('theme_style', get_stylesheet_directory_uri() . 'css/theme.css', array(), filemtime(get_stylesheet_directory() . 'css/theme.css'));
-} 
+/**
+ * Define Constants
+ */
+define( 'CHILD_THEME_ASTRA_CHILD_VERSION', '1.0.0' );
+
+/**
+ * Enqueue styles
+ */
+function child_enqueue_styles() {
+
+	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+	wp_enqueue_style('theme_style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . 'css/theme.css'));
+
+}
+
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+add_action('wpcf7_before_send_mail', 'add_separator_between_forms');
+
+function add_separator_between_forms($contact_form) {
+    // form seperator vertical
+    $separator_html = '<div class="uagb-container-inner-blocks-wrap"></div>';
+    
+    
+    $form_id_1 = 308; 
+    $form_id_2 = 284; 
+
+    
+    if ($contact_form->id == $form_id_1 || $contact_form->id == $form_id_2) {
+        echo $separator_html;
+    }
+}
