f16d9cf3314f3ac41539c1624570981af70dd151
zeliha <z_eymen@hotmail.com>
2024-01-09 09:01:56 +0100

    editing in images

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -57,7 +57,7 @@ add_filter( 'wp_nav_menu_items','add_admin_link', 10, 2 );
 // }
 
 function add_admin_link( $items, $args ) {
-    if (is_user_logged_in() && $args->theme_location == 'primary') {
+    if (is_user_logged_in() && $args -> theme_location == 'primary') {
         
         $items_array = explode('</li>', $items);
 
--- wp-content/uploads/uag-plugin/assets/0/uag-js-82-1704635373.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
-	UAGBForms.init( {"block_id":"ee64fe8e","reCaptchaEnable":false,"reCaptchaType":"v2","reCaptchaSiteKeyV2":"","reCaptchaSecretKeyV2":"","reCaptchaSiteKeyV3":"","reCaptchaSecretKeyV3":"","afterSubmitToEmail":"zeliha.e.ozyurt@hotmail.com","afterSubmitCcEmail":"","afterSubmitBccEmail":"","afterSubmitEmailSubject":"Commantaire de client","sendAfterSubmitEmail":true,"confirmationType":"message","hidereCaptchaBatch":false,"captchaMessage":"Veuillez remplir le captcha ci-dessus.","confirmationUrl":""}, '.uagb-block-ee64fe8e', 82 );
-});
- });
